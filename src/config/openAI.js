aabd230ac5c24a4157a235fc78d18b15ccba79f3
alionachernenko <errorroror75@gmail.com>
2024-04-22 00:27:10 +0300

    Initialize openAI

---
+++ src/config/openAI.js
@@ -0,0 +1,14 @@
+import OpenAI from "openai";
+import { Configuration, OpenAIApi } from "openai";
+
+const openai = new OpenAIApi(configuration);
+
+const configuration = new Configuration({
+  apiKey: process.env.REACT_APP_OPEN_AI_API_KEY,
+});
+
+const baseConfig = {
+  model: "gpt-3.5-turbo",
+  temperature: 5,
+  max_tokens: 1000,
+};
