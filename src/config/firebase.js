235bd29ae9684372f0a66fd2be086d8cea9004b9
Aliona Chernenko <108655520+alionachernenko@users.noreply.github.com>
2024-04-26 09:23:13 +0300

    Dev (#8)
    * Add dependencies - axios, react-query, redux, openAI, firebase
    
    * Initialize firebase
    
    * Initialize openAI
    
    * Add hooks and services files + firebase auth service
    
    * Add normalize and fix errors, comment openAi for awhile
    
    * eUkraine fonts (#1)
    
    * eUkraine fonts
    
    * add font-face for e-Ukraine fonts
    
    ---------
    
    Co-authored-by: Aliona Chernenko <108655520+alionachernenko@users.noreply.github.com>
    
    * Add basic common components (#2)
    
    * Onboarding (#3)
    
    * eUkraine fonts
    
    * add font-face for e-Ukraine fonts
    
    * temp commit before pull
    
    ---------
    
    Co-authored-by: sshaparenko <zshaparenkoz@gmail.com>
    
    * Authentication (#4)
    
    * WIP
    
    * Authentication (in progress)
    
    * Advantages (#6)
    
    * WIP
    
    * Fix index.js
    
    * Fix onboarding import
    
    * FIx api key
    
    ---------
    
    Co-authored-by: Mykola Shaparenko <102804572+sshaparenko@users.noreply.github.com>
    Co-authored-by: sshaparenko <zshaparenkoz@gmail.com>

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -6,13 +6,13 @@ import { getStorage } from "firebase/storage";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyAIFN4EjPoYZGzUcfYkloGkDc0vb9mPbN4",
+  authDomain: "native-future.firebaseapp.com",
+  projectId: "native-future",
+  storageBucket: "native-future.appspot.com",
+  messagingSenderId: "20640703860",
+  appId: "1:20640703860:web:9cd1fbf2c38ab01a5df821",
+  measurementId: "G-483W3Y05PT",
 };
 
 const app = initializeApp(firebaseConfig);
@@ -24,7 +24,3 @@ export const storage = getStorage(app);
 export const analytics = getAnalytics(app);
 
 auth.useDeviceLanguage();
-
-
-
-// export const appVerifier = window.recaptchaVerifier
